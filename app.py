950df898b264f221f246d340499ece5d85ca636c
res0nance <res0nance@users.noreply.github.com>
2017-08-26 16:59:40 +0800

    docker images are hard

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ def handle(msg):
     pprint.pprint(msg)
     if( 'sticker' in msg ):
         path = os.path.dirname(__file__)
-        filename = 'temp.webp'
-        bot.download_file(msg['sticker']['file_id'], os.path.join(path,filename))
-        img = Image.open('temp.webp')
-        img.save('temp.png')
-        image = client.upload_from_path('temp.png', anon=True)
+        bot.download_file(msg['sticker']['file_id'], os.path.join(path,'temp.webp'))
+        img = Image.open(os.path.join(path,'temp.webp'))
+        img.save(os.path.join(path,'temp.png'))
+        image = client.upload_from_path(os.path.join(path,'temp.png'), anon=True)
         bot.sendMessage(msg['chat']['id'], image['link'])
 
 
