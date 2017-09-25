7cb325a73e766b76ea0c1ff45b7a63ecb7f071e3
res0nance <res0nance@users.noreply.github.com>
2017-09-25 22:30:27 +0800

    Add more tests

--- test_app.py
+++ test_app.py
@@ -1,4 +1,12 @@
 import app
+from PIL import Image, features
+from imgurpython import ImgurClient
 
 def test_init():
     app.init()
+
+def test_pillow():
+	test_init()
+	img = Image.new('RGBA', (50,50))
+	img.save('test.jpg')
+	image = client.upload_from_path('test.jpg', anon=True)
