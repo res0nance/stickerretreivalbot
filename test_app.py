47813ab48ff42ee2e3c24e761b4c48898fdc0d26
res0nance <res0nance@users.noreply.github.com>
2017-09-25 22:32:41 +0800

    RGBA JPEGS was a good idea :lollipop:

--- test_app.py
+++ test_app.py
@@ -7,6 +7,6 @@ def test_init():
 
 def test_pillow():
 	test_init()
-	img = Image.new('RGBA', (50,50))
+	img = Image.new('RGB', (50,50))
 	img.save('test.jpg')
 	image = client.upload_from_path('test.jpg', anon=True)
