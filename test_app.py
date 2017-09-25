a0501cb72d8b0644d621b82f1db8e955bc9d2f27
res0nance <res0nance@users.noreply.github.com>
2017-09-25 22:35:14 +0800

    Steal client from app

--- test_app.py
+++ test_app.py
@@ -9,4 +9,9 @@ def test_pillow():
 	test_init()
 	img = Image.new('RGB', (50,50))
 	img.save('test.jpg')
-	image = client.upload_from_path('test.jpg', anon=True)
+
+def test_imgur():
+	test_pillow()
+	img = Image.open('test.jpg')
+	img.save('test.png')
+	image = app.client.upload_from_path('test.png', anon=True)
